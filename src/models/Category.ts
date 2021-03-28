2352307efffae9440dc6c3c142fb219f78932910
Flavio Milani <milanidesign7@gmail.com>
2021-03-28 00:44:13 -0300

    Desafio concluido

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,32 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Transaction from './Transaction';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @OneToMany(
+    () => Transaction,
+    (transaction: { category: any }) => transaction.category,
+  )
+  transaction: Transaction;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
